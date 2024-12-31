dfa6c7d5be6e5ed51d39dd9e24170a8a890da188
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2024-12-31 13:46:26 +0530

    usedResponsive styles lg, added hamburger icon and added conditional rendering onClick hamburger on small screen.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,13 @@
+"use client"
+import { useState } from "react";
+import { Menu } from 'lucide-react';
+
+
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <header className="bg-black text-white rounded-full my-2 m-8">
             <nav>
@@ -6,8 +15,14 @@ function Navbar() {
                     {/* logo section */}
                     <a href="#" className="logo ml-10">NEXTZONE</a>
 
+                    {/* Hamburger Icon */}
+                    <button className="text-white text-2xl mr-4 lg:hidden" onClick={handleMenu}>
+                        <Menu />
+                    </button>
+
+
                     {/* navigation links */}
-                    <ul className="nav-links flex space-x-8 font-thin text-sm">
+                    <ul className={`nav-links flex flex-col font-thin text-sm lg:flex lg:flex-row lg:justify-between lg:items-center lg:space-x-8 ${isOpen ? "block" : "hidden"} `}>
                         <li> <a href="#">Home</a> </li>
                         <li> <a href="#">About Us</a> </li>
                         <li> <a href="#">Porfolio</a> </li>
@@ -16,8 +31,8 @@ function Navbar() {
                     </ul>
 
                     {/* action button */}
-                    <div className="action-buttons space-x-4 px-0 mr-4">
-                        <button className="icon-button  bg-orange-500 sm:bg-blue-500 hover:bg-orange-600 py-3 px-5 rounded-full border-black text-black font-semibold mr-0">↗</button>
+                    <div className={`action-buttons font-thin text-sm lg:flex lg:flex-row lg:justify-between lg:items-center lg:space-x-4 px-0 mr-4 ${isOpen ? "block" : "hidden"}`}>
+                        <button className="icon-button  bg-orange-500 hover:bg-orange-600 py-3 px-5 rounded-full border-black text-black font-semibold mr-0">↗</button>
                         <button className="cta-button bg-orange-500 px-7 py-3 rounded-full hover:bg-orange-600 text-black border-black font-semibold text-sm">REQUEST A PROPOSAL</button>
                     </div>
                 </div>
