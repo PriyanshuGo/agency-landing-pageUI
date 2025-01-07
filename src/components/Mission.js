4942cbeb7504f2f9cf49b2fb53303cc9c4b9d1da
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-01-07 15:28:23 +0530

    ChevronDown at rightside

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -9,7 +9,7 @@ function Section1() {
     }
 
     return (
-        <div className="box-border my-10 mx-4 rounded-3xl space-y-15 md:my-24 md md:flex md:flex-col md:px-16 lg:flex-row lg:justify-between lg:space-y-0 xl:my-30">
+        <div className="box-border my-10 mx-4 rounded-3xl space-y-15 md:my-24 md md:flex md:flex-col md:px-16 lg:flex-row lg:justify-between lg:space-y-0 xl:my-32">
             <div className="space-y-5 mx-5 sm:space-y-12 ">
                 <h1 className="text-3xl font-semibold lg:text-5xl !leading-snug ">
                     WE SOLVE ALL OF THE                                                                     <br />
@@ -22,20 +22,20 @@ function Section1() {
 
                 <div className="flex flex-col lg:flex-row lg:space-y-0 ">
                     <div className='flex-1 lg:basis-1/3 px-2'>
-                        <p className="font-bold text-3xl xl:text-6xl xl:font-normal">250+</p>
+                        <p className="lg:font-bold text-3xl xl:text-6xl xl:font-normal">250+</p>
                         <span className="text-xs">
                             Successsful Websites Designed And Developed
                         </span>
                     </div>
                     <div className='flex-1 lg:basis-1/3 px-2'>
-                        <p className="font-bold text-3xl xl:text-6xl xl:font-normal">9+</p>
+                        <p className="lg:font-bold text-3xl xl:text-6xl xl:font-normal">9+</p>
                         <span className="text-xs">
                             Years in Business As A Top New York City Design Agency. Focusing
                             On Web Design & Development
                         </span>
                     </div>
                     <div className='flex-1 lg:basis-1/3 px-2'>
-                        <p className="font-bold text-3xl xl:text-6xl xl:font-normal">99%</p>
+                        <p className="lg:font-bold text-3xl xl:text-6xl xl:font-normal">99%</p>
                         <span className="text-xs">
                             Client Satisfaction On Web Design Work With Our Agency
                         </span>
@@ -47,8 +47,8 @@ function Section1() {
             {/* MISSION BLOCK */}
             <div className="mx-5 space-y-5 my-5 box-border lg:space-y-10 items-end lg:pointer-events-none">
                 <div className='py-4 border-t w-full xl:w-80'>
-                    <button onClick={() => handleToggle("mission")} className="flex">
-                        <p className="font-bold text-2xl">+OUR MISSION</p>
+                    <button onClick={() => handleToggle("mission")} className="flex w-full justify-between">
+                        <p className="lg:font-bold text-2xl">+OUR MISSION</p>
                         <ChevronDown
                             className={` ${isToggle === "mission" ? 'transform rotate-180' : ''
                                 } transition-all duration-200 lg:hidden `}
@@ -57,8 +57,8 @@ function Section1() {
                     <p className={`${isToggle === "mission" ? "block" : "hidden"} lg:block `}>Next Zone is a full service Digital agency working at the intersection design, technology and human interaction</p>
                 </div>
                 <div className='py-4 border-t w-full xl:w-80'>
-                    <button onClick={() => handleToggle("vision")} className="flex  ">
-                        <p className="font-bold text-2xl">+OUR VISION</p>
+                    <button onClick={() => handleToggle("vision")} className="flex w-full justify-between ">
+                        <p className="lg:font-bold text-2xl">+OUR VISION</p>
                         <ChevronDown
                             className={` ${isToggle === "vision" ? 'transform rotate-180' : ''
                                 } transition-all duration-200 lg:hidden`}
