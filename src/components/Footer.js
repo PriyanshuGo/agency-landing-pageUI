bec24308d3a9ebac40b8de7d8856a8bb0c629c87
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-01-24 16:08:03 +0530

    Implement light/dark mode theming with CSS variables and Tailwind CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mail } from 'lucide-react';
+
 
 function Footer() {
     const footLinks = [
@@ -11,16 +13,19 @@ function Footer() {
     return (
         <div className='my-10 mx-5 flex flex-col text-white bg-black rounded-md lg:flex-row lg:justify-around'>
             <div className='mx-5 md:mx-16 lg:mx-20 text-center'>
-                <div>NEXTZONE</div>
+                <div className=''>NEXTZONE</div>
                 <div className='text-sm'>Your One-Stop IT Partner: Branding, Web Development, and Tailored Solutions for Every Need</div>
             </div>
-            <div className='mx-5 md:mx-16 lg:mx-20 text-center'>
-                <div>agencyfolio@gmail.com</div>
+            <div className='mx-5 md:mx-16 lg:mx-20 '>
+                <div className='flex w-full justify-center'>  
+                    <Mail />
+                    agencyfolio@gmail.com
+                </div>
                 <ul className='flex flex-col sm:flex-row sm:justify-evenly items-center'>
                     {footLinks.map((el, index) => (
                         <li key={index}>
                             <Link href={el.href}>
-                            {el.lable}
+                                {el.lable}
                             </Link>
                         </li>
                     ))}
